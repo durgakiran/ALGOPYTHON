bc82540705a6f4c56fcbef77e43a0e69064007fd
durgakiran <gatemefuture@gmail.com>
2019-07-31 19:56:44 +0530

    better implementation of gcd

--- gcd.py
+++ gcd.py
@@ -22,6 +22,35 @@ def naiveGcd(a, b):
     return cf[-1]
 
 
+#improved gcd
+def improvedGcd(a, b):
+    cf = []
+
+    for i in range(1, min(a, b) + 1):
+        if a % i == 0 and b % i == 0:
+            cf.append(i)
+    
+    return cf[-1]
+
+
+
 gcd = naiveGcd(14, 63)
+betterGcd = improvedGcd(14, 63)
 
 print(gcd)
+print(gcd)
+
+
+#even better gcd
+def improvedplusGcd(a, b):
+    i = min(a, b)
+
+    while i > 0:
+        if a % i == 0 and b % i == 0:
+            return i
+        else:
+            i = i - 1
+
+
+betterPlusGcd = improvedGcd(14, 63)
+print(betterPlusGcd)
